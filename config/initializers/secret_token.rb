3a31a76411aca267b02aa827858a7507de3f5071
Evgeniy <litovkaea@mail.ru>
2013-05-30 15:27:08 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemonstrashkaApp::Application.config.secret_token = 'cd2ec7fe2df563fe1ce8e1a55f3598ddc096348d688bf85fcec24884f4c763d82246e4ffcce35a0f4ee0581c861a3219387ae9e17bd1a4a5320469dbef413b7f'
